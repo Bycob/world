b36d97b7c9ff20a32ef2ff9ad2a02b331ca4760f
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2017-04-07 21:41:50 +0200

    Tests pour la relief map

---
+++ tests/test_reliefmap.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdexcept>
+
+void testReliefMap(int, char**);
+
+int main(int argc, char** argv) {
+	testReliefMap(argc, argv);
+}
+
+void testReliefMap(int argc, char** argv) {
+	std::cout << "Not yet implemented" << std::endl;
+}
