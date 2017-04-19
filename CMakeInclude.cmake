63fe4af76bdf3368e121e19160c14791ed78a8e4
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2017-04-20 00:11:07 +0200

    Modules CMake

---
+++ CMakeInclude.cmake
@@ -0,0 +1,17 @@
+
+include (GenerateExportHeader)
+
+# Fonction tirée de : http://stackoverflow.com/questions/31422680/how-to-set-visual-studio-filters-for-nested-sub-directory-using-cmake
+function(assign_source_group)
+    foreach(_source IN ITEMS ${ARGN})
+        if (IS_ABSOLUTE "${_source}")
+            file(RELATIVE_PATH _source_rel "${CMAKE_CURRENT_SOURCE_DIR}" "${_source}")
+        else()
+            set(_source_rel "${_source}")
+        endif()
+
+        get_filename_component(_source_path "${_source_rel}" PATH)
+        string(REPLACE "/" "\\" _source_path_msvc "${_source_path}")
+        source_group("${_source_path_msvc}" FILES "${_source}")
+    endforeach()
+endfunction(assign_source_group)
