8e3940a22f6d7accdc579a48be062c507f525584
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2018-03-31 18:31:02 +0200

    [2018 spring rework] Changements dans les CMakeLists. Desormais l'absence d'une dependance ne bloque plus la generation des fichiers de build. Les variables et options utilisateurs ont ete legerement remaniees.

--- cmake/WorldDependencies.cmake
+++ cmake/WorldDependencies.cmake
@@ -1,15 +1,31 @@
 
-if (${WORLD_USE_OPENCV})
-    message("-- Use OpenCV")
-    find_package(OpenCV REQUIRED)
+# External dependencies :
+# For each dependency a variable WORLD_FOUND_[NAME] is created, indicating if we can use this dependency or not
+# - OpenCV
+
+set(WORLD_HAS_OPENCV OFF)
+
+if (${WORLD_BUILD_OPENCV_MODULES})
+    find_package(OpenCV QUIET)
 
     if(${OpenCV_FOUND})
+		message("-- Found OpenCV")
         include_directories(${OpenCV_INCLUDE_DIRS})
-        add_definitions(-DUSE_OPENCV)
     endif()
+
+	set(WORLD_HAS_OPENCV ${OpenCV_FOUND})
 endif()
 
+# - Irrlicht
+
+set(WORLD_HAS_IRRLICHT OFF)
+
 if (${WORLD_BUILD_WORLD3D})
-    find_package(Irrlicht REQUIRED)
-    include_directories(${IRRLICHT_INCLUDE_DIRS})
+    find_package(Irrlicht QUIET)
+
+	if (${IRRLICHT_FOUND})
+		include_directories(${IRRLICHT_INCLUDE_DIRS})
+	endif()
+
+	set(WORLD_HAS_IRRLICHT ${IRRLICHT_FOUND})
 endif()
