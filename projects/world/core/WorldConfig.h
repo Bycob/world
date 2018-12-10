33ed610254392bc40c99e41f1d3c76c5830eac68
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2018-12-10 23:07:33 +0100

    Fixed compilation on windows after refactoring

--- projects/world/core/WorldConfig.h
+++ projects/world/core/WorldConfig.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #if defined(_WIN32) && !defined(DEBUG_NO_EXPORTS)
-#if defined(worldapi_EXPORTS)
+// TODO rename WORLDAPI_EXPORT -> WORLD_EXPORT
+#if defined(world_EXPORTS)
 #define WORLDAPI_EXPORT __declspec(dllexport)
 #else
 #define WORLDAPI_EXPORT __declspec(dllimport)
-#endif /* WorldAPI_EXPORTS */
+#endif /* World_EXPORTS */
 #else  /* defined (_WIN32) */
 #define WORLDAPI_EXPORT
 #endif
