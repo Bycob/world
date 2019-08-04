8a8af834e4de02d5a54954b686fd56917d7506c4
Louis Jean <ljean@etud.insa-toulouse.fr>
2019-08-04 21:39:09 +0200

    Fix control issues on character controller

--- projects/peace/Assets/Scripts/Character.cs
+++ projects/peace/Assets/Scripts/Character.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public float ratio = 0.2f;
+    public float ratio = 1.0f;
     public float speed = 0.07f;
 
     private bool lockCursor;
@@ -21,7 +21,7 @@ public class Character : MonoBehaviour
     {
 
         // view rotation
-        Vector3 deltaPos = Input.mousePosition - prevMousePosition;
+        Vector2 deltaPos = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         transform.Rotate(new Vector3(0, deltaPos.x * ratio, 0), Space.World);
         transform.Rotate(new Vector3(- deltaPos.y * ratio, 0, 0), Space.Self);
 
@@ -37,6 +37,7 @@ public class Character : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             lockCursor = !lockCursor;
+            Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
         }
         
         prevMousePosition = Input.mousePosition;
