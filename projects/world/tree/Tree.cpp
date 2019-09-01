6f6e0b3216943d20a9f5df78cf6759fb591075f0
Louis Jean <ljean@etud.insa-toulouse.fr>
2019-09-01 23:11:55 +0200

    Move simple tree mesh from TreeGroup to Tree

--- projects/world/tree/Tree.cpp
+++ projects/world/tree/Tree.cpp
@@ -4,6 +4,7 @@
 
 #include "world/core/IResolutionModel.h"
 #include "world/assets/SceneNode.h"
+#include "world/assets/MeshOps.h"
 
 namespace world {
 class PTree {
@@ -37,17 +38,40 @@ const Mesh &Tree::getLeavesMesh() const { return _leavesMesh; }
 
 Mesh &Tree::leavesMesh() { return _leavesMesh; }
 
-void Tree::collect(ICollector &collector, const IResolutionModel &explorer,
+void Tree::collect(ICollector &collector,
+                   const IResolutionModel &resolutionModel,
                    const ExplorationContext &ctx) {
 
-    // Generation
-    if (!_generated) {
-        generateBase();
+    double resolution = resolutionModel.getResolutionAt({}, ctx);
+
+    const double SIMPLE_RES = 1;
+    const double BASE_RES = 5;
+
+    vec3d posOffset{};
+    ItemKey trunkKey{"1"};
+    ItemKey leavesKey{"2"};
+
+    if (resolution < SIMPLE_RES) {
+        return;
+    } else if (resolution < BASE_RES) {
+        if (_simpleTrunk.getVerticesCount() == 0)
+            generateSimpleMeshes();
+
+        trunkKey = {"s1"};
+        leavesKey = {"s2"};
+    } else {
+        if (!_generated) {
+            generateBase();
+        }
+        posOffset = ctx.hasEnvironment()
+                        ? ctx.getEnvironment().findNearestFreePoint(
+                              {}, {0, 0, 1}, resolution, ctx)
+                        : vec3d{};
     }
 
     // Collection
-    SceneNode trunk(ctx({"1"}).str());
-    SceneNode leaves(ctx({"2"}).str());
+    SceneNode trunk(ctx(trunkKey).str());
+    SceneNode leaves(ctx(leavesKey).str());
 
     // Material
     Material leavesMat("leaves");
@@ -67,12 +91,75 @@ void Tree::collect(ICollector &collector, const IResolutionModel &explorer,
             materialsChannel.put({"2"}, leavesMat, ctx);
         }
 
-        meshChannel.put({"1"}, _trunkMesh, ctx);
-        meshChannel.put({"2"}, _leavesMesh, ctx);
+        if (resolution < BASE_RES) {
+            meshChannel.put(trunkKey, _simpleTrunk, ctx);
+            meshChannel.put(leavesKey, _simpleLeaves, ctx);
+        } else {
+            meshChannel.put(trunkKey, _trunkMesh, ctx);
+            meshChannel.put(leavesKey, _leavesMesh, ctx);
+        }
 
-        objectsChannel.put({"1"}, trunk, ctx);
-        objectsChannel.put({"2"}, leaves, ctx);
+        trunk.setPosition(posOffset);
+        leaves.setPosition(posOffset);
+        objectsChannel.put(trunkKey, trunk, ctx);
+        objectsChannel.put(leavesKey, leaves, ctx);
     }
+}
+
+void Tree::generateSimpleMeshes() {
+    // trunk
+    // TODO utiliser le générateur d'arbres pour générer une version low
+    // poly du tronc avec peu de branches.
+    vec3d trunkBottom{};
+    vec3d trunkTop = trunkBottom + vec3d{0, 0.3, 2};
+    double trunkRadius = 0.2;
+
+    for (int i = 0; i < 3; ++i) {
+        double angle = M_PI * 2 * i / 3;
+        vec3d shift{cos(angle) * trunkRadius, sin(angle) * trunkRadius, 0};
+        _simpleTrunk.newVertex(trunkBottom + shift);
+        _simpleTrunk.newVertex(trunkTop + shift);
+        int ids[][3] = {{2 * i, (2 * i + 2) % 6, 2 * i + 1},
+                        {(2 * i + 2) % 6, (2 * i + 2) % 6 + 1, 2 * i + 1}};
+        _simpleTrunk.newFace(ids[0]);
+        _simpleTrunk.newFace(ids[1]);
+    }
+
+
+    // leaves
+    // TODO utiliser l'algorithme "QuickHull" pour générer des enveloppes
+    // convexes
+    vec3d leavesCenter = trunkTop;
+    float radius[] = {0.8f, 1.5f, 1.2f, 0.4f};
+    float height[] = {-0.35f, -0.05f, 0.5f, 0.9f};
+
+    const int ringCount = 4;
+    const int segmentCount = 7;
+
+    for (int i = 0; i < ringCount; ++i) {
+        for (int j = 0; j < segmentCount; ++j) {
+            double angle = M_PI * 2 * j / segmentCount;
+            vec3d vert =
+                leavesCenter + vec3d{cos(angle) * radius[i],
+                                     sin(angle) * radius[i], height[i]};
+            _simpleLeaves.newVertex(vert);
+
+            if (i != ringCount - 1) {
+                int ringOffset = segmentCount * i;
+                int totalOffset = ringOffset;
+                int ids[][3] = {{ringOffset + j, ringOffset + (j + 1) % 7,
+                                 ringOffset + segmentCount + j},
+                                {ringOffset + (j + 1) % 7,
+                                 ringOffset + segmentCount + (j + 1) % 7,
+                                 ringOffset + segmentCount + j}};
+                _simpleLeaves.newFace(ids[0]);
+                _simpleLeaves.newFace(ids[1]);
+            }
+        }
+    }
+
+    MeshOps::recalculateNormals(_simpleTrunk);
+    MeshOps::recalculateNormals(_simpleLeaves);
 }
 
 void Tree::generateBase() {
--- projects/world/tree/TreeGroup.cpp
+++ projects/world/tree/TreeGroup.cpp
@@ -43,148 +43,26 @@ void TreeGroup::collect(ICollector &collector,
         return;
     }
 
-    if (_trunksMesh.getVerticesCount() == 0 &&
-        _leavesMesh.getVerticesCount() == 0) {
-        regenerateGroup(ctx);
-    }
-
-    _trunksMesh.clearFaces();
-    _leavesMesh.clearFaces();
-
     for (TreeData &treeData : _internal->_trees) {
         double resolution =
             resolutionModel.getResolutionAt(treeData._position, ctx);
 
-        if (resolution > 5) {
-            // Generate tree
-            if (treeData._tree == nullptr) {
-                treeData._tree = std::make_unique<Tree>();
+        if (treeData._tree == nullptr) {
+            treeData._tree = std::make_unique<Tree>();
 
-                if (ctx.hasEnvironment()) {
-                    treeData._tree->setPosition3D(
-                        ctx.getEnvironment().findNearestFreePoint(
-                            treeData._position, {0, 0, 1}, 5, ctx));
-                } else {
-                    treeData._tree->setPosition3D(treeData._position);
-                }
-                configTree(*treeData._tree);
+            if (ctx.hasEnvironment()) {
+                treeData._tree->setPosition3D(
+                    ctx.getEnvironment().findNearestFreePoint(
+                        treeData._position, {0, 0, 1}, 1, ctx));
+            } else {
+                treeData._tree->setPosition3D(treeData._position);
             }
-
-            collectChild(NodeKeys::fromUint(treeData._id), *treeData._tree,
-                         collector, resolutionModel, ctx);
-
-        } else if (resolution > 1) {
-            // Add simplified version to group mesh
-            for (auto &face : treeData._trunkFaces) {
-                _trunksMesh.addFace(face);
-            }
-
-            for (auto &face : treeData._leavesFaces) {
-                _leavesMesh.addFace(face);
-            }
-        }
-    }
-
-    // Collect group meshes
-    if (_trunksMesh.getFaceCount() != 0 || _leavesMesh.getFaceCount() != 0) {
-
-        MeshOps::recalculateNormals(_trunksMesh);
-        MeshOps::recalculateNormals(_leavesMesh);
-
-        if (collector.hasChannel<SceneNode>() && collector.hasChannel<Mesh>()) {
-            auto &meshChannel = collector.getChannel<Mesh>();
-
-            auto &objChannel = collector.getChannel<SceneNode>();
-            std::string prefix{
-                (_internal->_updateParity = !_internal->_updateParity) ? "u"
-                                                                       : ""};
-
-            meshChannel.put({"1"}, _trunksMesh, ctx);
-            meshChannel.put({"2"}, _leavesMesh, ctx);
-
-            SceneNode trunksObj(ctx({"1"}).str());
-            SceneNode leavesObj(ctx({"2"}).str());
-
-            if (collector.hasChannel<Material>()) {
-                Material trunkMaterial("trunk");
-                trunkMaterial.setKd(0.5, 0.2, 0);
-                Material leafMaterial("leaf");
-                leafMaterial.setKd(0.4, 0.9, 0.4);
-
-                auto &matChannel = collector.getChannel<Material>();
-                trunksObj.setMaterialID(ctx.mutateKey({"1"}).str());
-                leavesObj.setMaterialID(ctx.mutateKey({"2"}).str());
-
-                matChannel.put({"1"}, trunkMaterial, ctx);
-                matChannel.put({"2"}, leafMaterial, ctx);
-            }
-
-            objChannel.put({prefix + "1"}, trunksObj, ctx);
-            objChannel.put({prefix + "2"}, leavesObj, ctx);
-        }
-    }
-}
-
-void TreeGroup::regenerateGroup(const ExplorationContext &ctx) {
-    for (TreeData &treeData : _internal->_trees) {
-        // trunk
-        // TODO utiliser le générateur d'arbres pour générer une version low
-        // poly du tronc avec peu de branches.
-        vec3d trunkBottom = ctx.hasEnvironment()
-                                ? ctx.getEnvironment().findNearestFreePoint(
-                                      treeData._position, {0, 0, 1}, 1, ctx)
-                                : treeData._position;
-        vec3d trunkTop = trunkBottom + vec3d{0, 0.3, 2};
-        double trunkRadius = 0.2;
-        int startIndex = _trunksMesh.getVerticesCount();
-
-        for (int i = 0; i < 3; ++i) {
-            double angle = M_PI * 2 * i / 3;
-            vec3d shift{cos(angle) * trunkRadius, sin(angle) * trunkRadius, 0};
-            _trunksMesh.newVertex(trunkBottom + shift);
-            _trunksMesh.newVertex(trunkTop + shift);
-            int ids[][3] = {{startIndex + 2 * i, startIndex + (2 * i + 2) % 6,
-                             startIndex + 2 * i + 1},
-                            {startIndex + (2 * i + 2) % 6,
-                             startIndex + (2 * i + 2) % 6 + 1,
-                             startIndex + 2 * i + 1}};
-            treeData._trunkFaces.emplace_back(ids[0]);
-            treeData._trunkFaces.emplace_back(ids[1]);
+            configTree(*treeData._tree);
         }
 
-
-        // leaves
-        // TODO utiliser l'algorithme "QuickHull" pour générer des enveloppes
-        // convexes
-        vec3d leavesCenter = trunkTop;
-        float radius[] = {0.8f, 1.5f, 1.2f, 0.4f};
-        float height[] = {-0.35f, -0.05f, 0.5f, 0.9f};
-        startIndex = _leavesMesh.getVerticesCount();
-
-        const int ringCount = 4;
-        const int segmentCount = 7;
-
-        for (int i = 0; i < ringCount; ++i) {
-            for (int j = 0; j < segmentCount; ++j) {
-                double angle = M_PI * 2 * j / segmentCount;
-                vec3d vert =
-                    leavesCenter + vec3d{cos(angle) * radius[i],
-                                         sin(angle) * radius[i], height[i]};
-                _leavesMesh.newVertex(vert);
-
-                if (i != ringCount - 1) {
-                    int ringOffset = segmentCount * i;
-                    int totalOffset = startIndex + ringOffset;
-                    int ids[][3] = {{totalOffset + j, totalOffset + (j + 1) % 7,
-                                     totalOffset + segmentCount + j},
-                                    {totalOffset + (j + 1) % 7,
-                                     totalOffset + segmentCount + (j + 1) % 7,
-                                     totalOffset + segmentCount + j}};
-                    treeData._leavesFaces.emplace_back(ids[0]);
-                    treeData._leavesFaces.emplace_back(ids[1]);
-                }
-            }
-        }
+        // Generate tree
+        collectChild(NodeKeys::fromUint(treeData._id), *treeData._tree,
+                     collector, resolutionModel, ctx);
     }
 }
 
